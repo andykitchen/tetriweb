f49e48803928490e92775da5fdc21623e1d9af3b
Andy Kitchen <kitchen.andy+git@gmail.com>
2011-06-11 22:18:09 +1000

    Shape array

--- main.go
+++ main.go
@@ -8,15 +8,47 @@ import (
 const (
 	WIDTH  = 10
 	HEIGHT = 20
+	SHAPE_W = 4
+	SHAPE_H = 4
 )
 
 type Board struct {
 	cells [WIDTH*HEIGHT]int
+	sx, sy int
 }
 
 func (b *Board) getCell(i, j int) int {
 	return b.cells[i*WIDTH + j]
 }
+
+// func (b *Board) rowFull(i int) (r bool) {
+// 	r = true
+// 	
+// 	for j := 0; j < WIDTH; j++ {
+// 		r &&= b.getCell(i, j) > 0
+// 		
+// 		if !r {
+// 			break
+// 		}
+// 	}
+// 	
+// 	return
+// }
+
+// func (b *Board) removeRow(i int) (r bool) {
+// 	r = true
+// 	
+// 	for j := 0; j < WIDTH; j++ {
+// 		r &&= b.getCell(i, j) > 0
+// 		
+// 		if !r {
+// 			break
+// 		}
+// 	}
+// 	
+// 	return
+// }
+
 
 func (b *Board) String() (s string) {
 	for i := 0; i < HEIGHT; i++ {
