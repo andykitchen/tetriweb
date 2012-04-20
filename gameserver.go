eacd9b4f217455dd702f4f9e022e5f12f1c1129b
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-20 12:09:01 +1000

    Use publish subscribe pattern for board states

--- gameserver.go
+++ gameserver.go
@@ -1,103 +1,127 @@
 package main
 
 import (
-	"fmt"
 	"websocket"
-	"json"
 	"http"
-	"syscall"
+	"fmt"
+	"time"
+	// "syscall"
 )
 
-var (
-	counter    int
-	game       *Game
-)
+type Conn struct {
+	*websocket.Conn
+	done chan int
+}
 
-func GameServer(ws *websocket.Conn) {
-	if counter > 10 {
-		fmt.Println("Max number of players reached")
-		return
+func (c *Conn) Close() {
+	c.done <- 1
+}
+
+func newWebSocketHandler(connections chan Conn) websocket.Handler {
+	return func(ws *websocket.Conn) {
+		done := make(chan int)
+		connections <- Conn{ws, done}
+		<- done
 	}
+}
 
-	p := new(Player)
-	p.id = counter
-	p.name = "Test"
+type Broadcaster struct {
+	subscriptions chan chan GameSession
+	writes []chan GameSession
+	send chan GameSession
+}
 
-	session := game.AddPlayer(p)
-	session.Start()
-
-	fmt.Println("New player: ", p.id)
-	counter += 1
-
-	buf := make([]byte, 512)
-
+func newBroadcaster() (b *Broadcaster) {
+	b = new(Broadcaster)
+	b.subscriptions = make(chan chan GameSession)
+	b.writes = make([]chan GameSession, 0)
+	b.send = make(chan GameSession)
+	
 	go func() {
 		for {
-			n, err := ws.Read(buf)
-			if err != nil {
-				fmt.Println("Websocket read error: ", err.String())
-				return // end goroutine
-			}
-
-			if n > 0 {
-				key := string(buf[:n])
-				session.HandleKey(key)
+			select {
+			case sub := <- b.subscriptions:
+				b.writes = append(b.writes, sub)
+			case data := <- b.send:
+				for _, x := range b.writes {
+					x <- data
+				}
 			}
 		}
 	}()
-
-	for {
-		for i := 0; i < len(game.sessions); i++ {
-			_, err := ws.Write(game.sessions[i].Encode())
-			if err != nil {
-				fmt.Println("Websocket write error: ", err.String())
-				return // end handler
-			}
-		}
-		session.Tick()
-
-		syscall.Sleep(100000000)
-	}
-}
-
-type PlayerBoard struct {
-	CurrentState []int
-	PlayShape    *Shape
-	NextShape    *Shape
-}
-
-
-func (b *Board) ToJson() (result []byte) {
-	player_board := new(PlayerBoard)
-	player_board.PlayShape = b.play_shape
-	player_board.NextShape = b.next_shape
-	player_board.CurrentState = b.GetBoardState()
-
-	result, _ = json.Marshal(player_board)
+	
 	return
 }
 
-// type playerHandler struct {
-// 	player Player
-// 	keys chan string
-// 	board 
-// }
-// 
-// func gameserver(new_players chan Player) {
-// 	
-// }
+func (b *Broadcaster) Subscribe(sub chan GameSession) {
+	b.subscriptions <- sub
+}
+
+func gameserver(connections chan Conn) {
+	ticker := time.NewTicker(1e8)
+	sessions := make([]GameSession, 0)
+	broadcast := newBroadcaster()
+	player_count := 0
+
+	for {
+		select {
+		case conn := <- connections:
+			session := GameSession{&Player{player_count, "foo"}, &Board{}}
+			session.Start() // required initialization
+			sessions = append(sessions, session)
+			player_count++
+			
+			go func() {
+				buf := make([]byte, 512)
+				
+				for {
+					n, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("Websocket read error: ", err.String())
+						return // end goroutine
+					}
+
+					if n > 0 {
+						key := string(buf[:n])
+						fmt.Print(key)
+						session.HandleKey(key)
+					}
+				}
+			}()
+			
+			session_chan := make(chan GameSession)
+			broadcast.Subscribe(session_chan)
+
+			go func() {
+				for {
+					session := <- session_chan
+					_, err := conn.Write(session.Encode())
+					if err != nil {
+						fmt.Println("Websocket write error: ", err.String())
+						return // end goroutine
+					}
+				}
+			}()
+
+		case <- ticker.C:
+			for _, session := range sessions {
+				session.Tick()
+				broadcast.send <- session
+			}
+		}
+	}
+	
+	ticker.Stop()
+}
 
 func main() {
-	game = new(Game)
-	fmt.Println("starting game...")
+	connections := make(chan Conn)
 
-	if terminalFlag {
-		PlayTerminal()
-	} else {
-		http.Handle("/tetris", websocket.Handler(GameServer))
-		err := http.ListenAndServe(":4000", nil)
-		if err != nil {
-			panic("ListenAndServe: " + err.String())
-		}
+	go gameserver(connections)
+
+	http.Handle("/tetris", newWebSocketHandler(connections))
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.String())
 	}
 }
